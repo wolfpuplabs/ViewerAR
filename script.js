ea33799e5c21d88d4e4ebbcd6cbd4892de33fae6
wolfpuplabs <167528982+wolfpuplabs@users.noreply.github.com>
2024-06-06 01:25:37 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('upload').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const url = URL.createObjectURL(file);
+        const modelViewer = document.getElementById('model');
+        modelViewer.src = url;
+    }
+});
